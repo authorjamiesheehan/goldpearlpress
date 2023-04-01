658ac0dd3e2a01106c78eb5a90457185255d30b2
selunders <sethlunders@gmail.com>
2023-04-01 13:40:49 -0700

    Initial site upload

---
+++ js/home.js
@@ -0,0 +1,26 @@
+anime({
+    targets: '.hero div',
+    translateY: {
+        value: 20,
+        // delay: 400,
+        // easing: 'easeInOutQuart',
+        duration: 1000,
+    },
+    // translateY: {
+        // value: 100,
+        // duration: 5000,
+        // },
+        // translateX: 250,
+        // rotate: '1turn',
+        // backgroundColor: {
+            // value: 'rgba(0, 0, 0, 0.925)',
+            // duration: 10000,
+            // },
+    easing: 'easeInOutSine',
+    borderRadius: 8,
+    direction: 'reverse',
+    opacity: 0,
+    // looping: true,
+    duration: 500
+});
+
